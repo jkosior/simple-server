529194b2a21b282a121f7b2d6d899755694933ac
Jakub Kosior <jakubjkosior@gmail.com>
2020-02-12 17:40:03 +0100

    delete swagger

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
-import { useSwagger } from './server/use-swagger';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  useSwagger(app);
   await app.listen(process.env.PORT || 3000);
 }
 
